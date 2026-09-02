5c5bba838d4a2d290df6629b3c8433b761cc3cb1
Shahd-Barmawi <shahdbarmawi2003@gmail.com>
2026-09-02 11:12:40 +0300

    Add backend dynamic page section builder

--- plugins/training/services/controllers/Pages.php
+++ plugins/training/services/controllers/Pages.php
@@ -10,11 +10,14 @@ class Pages extends Controller
     public $implement = [
         \Backend\Behaviors\FormController::class,
         \Backend\Behaviors\ListController::class,
+        \Backend\Behaviors\RelationController::class,
     ];
 
     public $formConfig = 'config_form.yaml';
 
     public $listConfig = 'config_list.yaml';
+
+    public $relationConfig = 'config_relation.yaml';
 
     public $requiredPermissions = [
         'training.services.manage_pages',
